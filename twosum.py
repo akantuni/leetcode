5325977ff8ba09a01df1b9a3cd4f1a1dcabcfc0e
Alexander Kantuni <akantuni@gmail.com>
2022-11-27 18:54:29 -0500

    Delete twosum.py

--- twosum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        memo = {}
-
-        for i, num in enumerate(nums):
-            ind = memo.get(num)
-            if ind is not None:
-                return [ind, i]
-            memo.update({target - num: i})
