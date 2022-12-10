caedd45e32bab0569a81d140c36c24072ef3eeb9
Alexander Kantuni <akantuni@gmail.com>
2022-12-09 21:57:49 -0500

    Create sqrtx-v2.cpp

---
+++ 69/sqrtx-v2.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        for (long int i = 0; i <= x / 2 + 1; i++) {
+            if ((i * i) <= x && (i + 1) * (i + 1) > x) {
+                return i;
+            }
+        }
+        return 0;
+    }
+};
