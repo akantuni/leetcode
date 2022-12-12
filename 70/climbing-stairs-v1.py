61a79d6c6c16a4d0a37b252b69eace8b45b5e40e
Alexander Kantuni <akantuni@gmail.com>
2022-12-12 00:37:41 -0500

    Update and rename climbing-stairs-va.py to climbing-stairs-v1.py

--- 70/climbing-stairs-v1.py
+++ 70/climbing-stairs-v1.py
@@ -1,17 +1,6 @@
 class Solution:
     @lru_cache
     def climbStairs(self, n: int) -> int:
-        # 1: 1
-        # 2: 2
-        # 3: 3
-        # 4: 5
-        # 5: 8
-        # 6: 13
-        # 7: 21
-        # 8: 34
-        # 9: 55
-        # 10: 89
-
         if n <= 1:
             return 1
         
